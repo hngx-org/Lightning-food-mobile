c22470e6a6ee9afa6b1c3c58663688a1c8ebc407
Rayhan Awojobi <dexterousnorthpaw@gmail.com>
2023-09-20 17:39:07 +0100

    Screen Util initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:lightning_food_mobile/constants/app_colors.dart';
 
@@ -12,11 +13,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const Home(),
+    return ScreenUtilInit(
+      designSize: Size (300,300),
+      builder: (_, child){
+        return MaterialApp(
+          theme: ThemeData(
+            useMaterial3: true,
+          ),
+          home: const Home(),
+        );
+      },
+
     );
   }
 }
