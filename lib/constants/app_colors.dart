ffe55e6dfe44e2acb13192e5d70d10c64d6483fd
Rayhan Awojobi <dexterousnorthpaw@gmail.com>
2023-09-20 16:47:51 +0100

    "App Colors created"

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor{
+  AppColor._();
+
+  static Color primaryColor = Color(0xFF28289F);
+  static Color secondaryColor = Color(0xFFFFDB58);
+  static Color tetiaryColor = Color(0xFFFFA776);
+  static Color bgColor = Color(0xFFFFF9EA);
+  static Color plainBlack = Color(0xFF000000);
+  static Color black2 = Color(0xFF1E1B1C);
+  static Color black33 = Color(0xFF4C4C4C);
+  static Color lightBlack = Color(0xFF9C9C9C);
+  static Color pureWhite = Color(0xFFFFFFFF);
+  static Color white2 = Color(0xFFFDFDFD);
+  static Color white3 = Color(0xFFF0F0F0);
+}
