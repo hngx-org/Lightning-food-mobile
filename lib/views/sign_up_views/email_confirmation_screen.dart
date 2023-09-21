017d8a32c6b70c69e0ba6d63c99839db9aedf0cb
unknown <lawalridwan007@gmail.com>
2023-09-21 10:03:50 +0100

    create sign_up and email_confirmation screens

---
+++ lib/views/sign_up_views/email_confirmation_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lightning_food_mobile/constants/app_colors.dart';
+import 'package:lightning_food_mobile/constants/app_widgets.dart';
+import 'package:lightning_food_mobile/main.dart';
+
+class EmailConfirmationScreen extends StatefulWidget {
+  const EmailConfirmationScreen({super.key});
+
+  @override
+  State<EmailConfirmationScreen> createState() =>
+      _EmailConfirmationScreenState();
+}
+
+class _EmailConfirmationScreenState extends State<EmailConfirmationScreen> {
+  late TextEditingController _codeController;
+
+  @override
+  void initState() {
+    super.initState();
+    _codeController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                  child: Text(
+                    'Confirmation',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.h),
+                const Text(
+                  'Check your email for a confirmation code',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                const Text("We've sent a 6 digit code to mayacox@gmail.com."),
+                const Text(
+                    "The code expires shortly, so please enter it soon."),
+                SizedBox(height: 10.h),
+                const Text('Confirmation code'),
+                SizedBox(height: 5.h),
+                TextField(
+                  controller: _codeController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: 'e.g. kayjay@gmail.com',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 10,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5.h),
+                Container(
+                  height: 4,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(),
+                  ),
+                  child: Row(
+                    children: [
+                      const Expanded(flex: 1, child: SizedBox()),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: Theme.of(context).colorScheme.primary,
+                          width: 0,
+                        ),
+                      ),
+                      const Expanded(flex: 1, child: SizedBox()),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30.h),
+                AppButton(
+                  height: 50,
+                  width: double.infinity,
+                  buttonColor: AppColor.primaryColor,
+                  buttonText: 'Continue',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Home(),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 20.h),
+                const Center(
+                  child: Text("Can't find your code? Check your spam folder!"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _codeController.dispose();
+    super.dispose();
+  }
+}
