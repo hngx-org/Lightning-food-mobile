f5777d77219dc46a8ca211efcf2a6d1acb2e9414
unknown <lawalridwan007@gmail.com>
2023-09-22 21:23:29 +0100

    fix spacing on screens

--- lib/views/sign_up_views/sign_up_screen.dart
+++ lib/views/sign_up_views/sign_up_screen.dart
@@ -23,122 +23,120 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      fontSize: 20.sp,
-                      fontWeight: FontWeight.w600,
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        foregroundColor: AppColor.black2,
+        backgroundColor: AppColor.bgColor,
+        elevation: 0,
+        title: Text(
+          'Register',
+          style: TextStyle(
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome on board!',
+                style: TextStyle(
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 16.h),
+              Text(
+                "Create an account to enjoy the lightening employees's app.",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 28.h),
+              Text('Create Account', style: TextStyle(fontSize: 24.sp)),
+              SizedBox(height: 16.h),
+              const Text('To get started, we just need a few details from you'),
+              SizedBox(height: 22.h),
+              const Text('Email Address'),
+              SizedBox(height: 4.h),
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'e.g. kayjay@gmail.com',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.sp),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16.w,
+                    vertical: 10.h,
                   ),
                 ),
-                SizedBox(height: 5.h),
-                Text(
-                  'Welcome on board!',
-                  style: TextStyle(
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.w700,
-                  ),
+              ),
+              SizedBox(height: 12.h),
+              Container(
+                height: 4.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.sp),
+                  border: Border.all(),
                 ),
-                SizedBox(height: 10.h),
-                Text(
-                  "Create an account to enjoy the lightening employees's app.",
-                  style: TextStyle(fontSize: 16.sp),
-                ),
-                SizedBox(height: 10.h),
-                Text(
-                  'Create Account',
-                  style: TextStyle(fontSize: 24.sp)
-                ),
-                SizedBox(height: 10.h),
-                const Text(
-                    'To get started, we just need a few details from you'),
-                SizedBox(height: 10.h),
-                const Text('Email Address'),
-                SizedBox(height: 5.h),
-                TextField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: 'e.g. kayjay@gmail.com',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16.w,
-                      vertical: 10.h,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 5.h),
-                Container(
-                  height: 4.h,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 0,
-                        ),
-                      ),
-                      const Expanded(flex: 2, child: SizedBox()),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 84.h),
-                AppButton(
-                  height: 50.h,
-                  buttonColor: AppColor.primaryColor,
-                  width: double.infinity,
-                  buttonText: 'Continue',
-                  fontSize: 16.sp,
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const EmailConfirmationScreen(),
-                      ),
-                    );
-                  }, buttonTextColor: AppColor.pureWhite,
-                ),
-                SizedBox(height: 107.h),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Row(
                   children: [
-                    const Text('Have an account already?'),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginScreen(),
-                          ),
-                        );
-                      },
-                      child: const Text(
-                        'Log in',
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: AppColor.primaryColor,
+                        width: 0,
                       ),
                     ),
+                    const Expanded(flex: 2, child: SizedBox()),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              SizedBox(height: 84.h),
+              AppButton(
+                height: 50.h,
+                buttonColor: AppColor.primaryColor,
+                width: double.infinity,
+                buttonText: 'Continue',
+                fontSize: 16.sp,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const EmailConfirmationScreen(),
+                    ),
+                  );
+                },
+                buttonTextColor: AppColor.pureWhite,
+              ),
+              SizedBox(height: 107.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Have an account already?'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
